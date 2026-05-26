9f1c60362d010b39532beab9157eeb9587ba7d2c
Loukik Kumar <loukikkumar03@gmail.com>
2026-05-27 00:35:42 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (data) => {
+        io.emit('chat message', data);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log('Server running');
+});
